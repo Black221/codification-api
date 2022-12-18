71d024122ddb0eb71d4786735fc07f84c0057619
Black221 <bambabar02@gmail.com>
2022-12-18 15:35:56 +0000

    commit

--- config/database.js
+++ config/database.js
@@ -1,7 +1,13 @@
 const mongoose = require("mongoose");
 
-const uri = "mongodb+srv://LHackSRT:"+process.env.DB_PASS+"@hintscluster.yefi5qz.mongodb.net/"+process.env.DB_NAME;
 
-    mongoose.connect(uri, {useNewUrlParser: true, useUnifiedTopology: true})
-        .then(() => console.log('Connected to MDB'))
-        .catch(error => () => console.log('Connected error :', error));
+const uri = "mongodb+srv://codifHackUser:"+process.env.DB_PASS+"@codification.06cdi.mongodb.net/"+process.env.DB_NAME;
+
+
+
+const { MongoClient, ServerApiVersion } = require('mongodb');
+const client = new MongoClient(uri, { useNewUrlParser: true, useUnifiedTopology: true, serverApi: ServerApiVersion.v1 });
+client.connect(err => {
+  if (!err)
+    console.log("db working");
+});
