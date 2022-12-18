35194ef8143c733360bc2dd081c66cf8f212f1c4
LHackSRT <78321063+Black221@users.noreply.github.com>
2022-12-18 22:20:22 +0000

    Update database.js

--- config/database.js
+++ config/database.js
@@ -1,10 +1,8 @@
 const mongoose = require("mongoose");
 
 
-const uri = "mongodb+srv://codifHackUser:"+process.env.DB_PASS+"@codification.06cdi.mongodb.net/"+process.env.DB_NAME;
-
+const uri = "mongodb+srv://codifHackUser:xakOxfexNTmPpGVS@codification.06cdi.mongodb.net/db_codif?retryWrites=true&w=majority";
 
 mongoose.connect(uri, {useNewUrlParser: true, useUnifiedTopology: true})
         .then(() => console.log('Connected to MDB'))
-        .catch(error => () => console.log('Connected error :', error));
-
+        .catch((error) => console.log('Connected error :', error));
